0d090a10b4c0e9440511f59fb4ebe70e171d7a6d
AnalyticsForPleasure <shaycohen2003@gmail.com>
2024-03-02 21:43:49 +0200

    need to change several thins undr the relationship betwen loan purpose and Interest rate

--- Questions/Question_8/Question_8.py
+++ Questions/Question_8/Question_8.py
@@ -39,70 +39,159 @@ def categorize_interest_rate(rate):
 # input:
 # return value:
 # ***************************************************************************************************************
-def creating_the_dataframe_correlation_for_each_region(Region_selected_df,Region_selected):
-    max_int_rate = Region_selected_df['int_rate'].max()
-    min_int_rate = Region_selected_df['int_rate'].min()
-    unique_purpose = pd.unique(Region_selected_df['purpose'])
+def creating_the_dataframe_correlation_for_each_region(selected_df):#,Region_selected):
+    Regions = ['West', 'Southwest', 'Midwest', 'Southeast', 'Northeast']
 
-    purpose_his = Region_selected_df['purpose'].value_counts()
+    Region_selected = 'Midwest'
+    #Region_selected_df = selected_df.loc[selected_df['Region'] == Region_selected, :]
+    West_selected_df = selected_df.loc[selected_df['Region'] == 'West']
+    Southwest_selected_df = selected_df.loc[selected_df['Region'] == 'Southwest']
+    Midwest_selected_df = selected_df.loc[selected_df['Region'] == 'Midwest']
+    Southeast_selected_df = selected_df.loc[selected_df['Region'] == 'Southeast']
+    Northeast_selected_df = selected_df.loc[selected_df['Region'] == 'Northeast']
+
+
+    # max_int_rate = Region_selected_df['int_rate'].max()
+    # min_int_rate = Region_selected_df['int_rate'].min()
+    # unique_purpose = pd.unique(Region_selected_df['purpose'])
+
+    #purpose_his = Region_selected_df['purpose'].value_counts()
     print("*")
     # Changing the format of 'int_rate' column
-    Region_selected_df['int_rate'] *= 100
+    selected_df['int_rate'] *= 100
     # Define the labels for the bins
     interest_rate_list = ['0% - 4%', '4% - 8%', '8% - 12%', '12% - 16%', '16% - 20%', '20% - 24%', '24% - 28%']
 
     # Apply the custom function to categorize interest rates into groups
-    Region_selected_df['interest_rate_groups'] = Region_selected_df['int_rate'].apply(categorize_interest_rate)
+    selected_df['interest_rate_groups'] = selected_df['int_rate'].apply(categorize_interest_rate)
 
-    three_columns = Region_selected_df.loc[:,['interest_rate_groups',"int_rate","purpose"]]
+    three_columns = selected_df.loc[:,['interest_rate_groups',"int_rate","purpose", 'Region']]
     print('*')
 
+    # Filtering th X-axis by this closed list
     relevant_reasons_for_taking_loans = ['Debt consolidation', 'redit card', 'home improvement','house', 'major purchase', 'small business','car','wedding','medical']
-
     final_table = three_columns[three_columns['purpose'].isin(relevant_reasons_for_taking_loans)]
 
-    output_result= pd.crosstab(final_table["interest_rate_groups"],final_table["purpose"])
+    #output_result= pd.crosstab(final_table["interest_rate_groups"],final_table["purpose"])
 
     print('*')
 
 
-    return output_result
+    return final_table
 
 
 
 
 def creating_the_correlation_chart_for_each_region(final_table):
+
+    filtering_West_df = final_table.loc[final_table['Region'] == 'West']
+    df_West= pd.crosstab(filtering_West_df["interest_rate_groups"],filtering_West_df["purpose"])
+
+    filtering_Southwest_df = final_table.loc[final_table['Region'] == 'Southwest']
+    df_Southwest = pd.crosstab(filtering_Southwest_df["interest_rate_groups"], filtering_Southwest_df["purpose"])
+
+    filtering_Midwest_df = final_table.loc[final_table['Region'] == 'Midwest']
+    df_Midwest = pd.crosstab(filtering_Midwest_df["interest_rate_groups"], filtering_Midwest_df["purpose"])
+
+    filtering_Southeast_df = final_table.loc[final_table['Region'] == 'Southeast']
+    df_Southeast = pd.crosstab(filtering_Southeast_df["interest_rate_groups"], filtering_Southeast_df["purpose"])
+
+    filtering_Northeast_df = final_table.loc[final_table['Region'] == 'Northeast']
+    df_Northeast = pd.crosstab(filtering_Northeast_df["interest_rate_groups"], filtering_Northeast_df["purpose"])
+
+    print('*')
+
+
+
     fig = plt.figure(figsize=(15, 10))  # create figure
     gs = fig.add_gridspec(2, 3)
     gs.update(wspace=0.2, hspace=-0.4)
+    # Positioning each plot over the chart
     ax0 = fig.add_subplot(gs[0, 0])
     ax1 = fig.add_subplot(gs[0, 1])
-    # ax2 = fig.add_subplot(gs[0, 2])
-    # ax3 = fig.add_subplot(gs[1, 0])
-    # ax4 = fig.add_subplot(gs[1, 1])
-    # ax5 = fig.add_subplot(gs[1, 2])
+    #ax2 = fig.add_subplot(gs[0, 2])
+    ax3 = fig.add_subplot(gs[1, 0])
+    ax4 = fig.add_subplot(gs[1, 1])
+    ax5 = fig.add_subplot(gs[1, 2])
     background_color = "#fbfbfb"
     fig.patch.set_facecolor(background_color)  # figure background color
     ax0.set_facecolor(background_color)  # axes background color
     ax1.set_facecolor(background_color)  # axes background color
-    # ax2.set_facecolor(background_color)  # axes background color
-    # ax3.set_facecolor(background_color)  # axes background color
-    # ax4.set_facecolor(background_color)  # axes background color
-    # ax5.set_facecolor(background_color)  # axes background color
-    colors = ["#fbfbfb", "#4b4b4c", "#FFA500"]
+    #ax2.set_facecolor(background_color)  # axes background color
+    ax3.set_facecolor(background_color)  # axes background color
+    ax4.set_facecolor(background_color)  # axes background color
+    ax5.set_facecolor(background_color)  # axes background color
+    colors = [ "#4b4b4c", "#FFA500"]
+
+    # Reverse the order of colors
+    colors.reverse()
+
+    # Create a colormap using LinearSegmentedColormap
+    cmap = LinearSegmentedColormap.from_list('OrangeToGray', colors)
+
+    # Generate 5 distinct colors from the colormap
+    distinct_colors = [cmap(i) for i in np.linspace(0, 1, 50)]
+    print('*')
+
+
     colormap = matplotlib.colors.LinearSegmentedColormap.from_list("", colors)
     sns.heatmap(ax=ax0,
-                data=final_table,
+                data=df_West,
                 linewidths=.2,
                 square=True,
                 cbar_kws={"orientation": "horizontal"},
                 cbar=False,
-                cmap=colormap)
-    sns.heatmap(ax=ax1, data=final_table, linewidths=.1,
+                annot=True,  # Add annotations (numbers) over the cells
+                cmap=distinct_colors,
+                fmt='.3g',
+                annot_kws={"fontsize": 8,"fontweight": "bold"})
+    sns.heatmap(ax=ax1,
+                data=df_Southwest,
+                linewidths=.1,
                 square=True,
                 cbar_kws={"orientation": "horizontal"},
                 cbar=False,
-                cmap=colormap)
+                annot=True,  # Add annotations (numbers) over the cells
+                cmap=distinct_colors,
+                fmt='.3g',
+                annot_kws={"fontsize": 8,"fontweight": "bold"})
+    sns.heatmap(ax=ax5,
+                data=df_Midwest,
+                linewidths=.1,
+                square=True,
+                cbar_kws={"orientation": "horizontal"},
+                cbar=False,
+                annot=True,  # Add annotations (numbers) over the cells
+                cmap=distinct_colors,
+                fmt='.3g',
+                annot_kws={"fontsize": 8,"fontweight": "bold"})
+    sns.heatmap(ax=ax3,
+                data=df_Southeast,
+                linewidths=.1,
+                square=True,
+                cbar_kws={"orientation": "horizontal"},
+                cbar=False,
+                annot=True,  # Add annotations (numbers) over the cells
+                cmap=distinct_colors,
+                fmt='.3g',
+                annot_kws={"fontsize": 8,"fontweight": "bold"})
+    sns.heatmap(ax=ax4,
+                data=df_Northeast,
+                linewidths=.1,
+                square=True,
+                cbar_kws={"orientation": "horizontal"},
+                cbar=False,
+                annot=True,  # Add annotations (numbers) over the cells
+                cmap=distinct_colors,
+                fmt='.3g',
+                annot_kws={"fontsize": 8,"fontweight": "bold"})
+    # sns.heatmap(ax=ax5,
+    #             data=df_Northeast,
+    #             linewidths=.1,
+    #             square=True,
+    #             cbar_kws={"orientation": "horizontal"},
+    #             cbar=False,
+    #             cmap=colormap)
 
     ax0.text(-2.5, -1.7,
              'Exploring the Relationship Between Loan Purpose and Interest Rates',
@@ -112,38 +201,45 @@ def creating_the_correlation_chart_for_each_region(final_table):
              fontsize=13, fontweight='light', fontfamily='serif')
     ax0.text(0, -0.2, 'West', fontsize=15, fontweight='bold', fontfamily='serif')
     ax1.text(0, -0.2, 'Southwest', fontsize=15, fontweight='bold', fontfamily='serif')
+    #ax2.text(0, -0.2, 'Midwest', fontsize=15, fontweight='bold', fontfamily='serif')
+    ax3.text(0, -0.2, 'Southeast', fontsize=15, fontweight='bold', fontfamily='serif')
+    ax4.text(0, -0.2, 'Northeast', fontsize=15, fontweight='bold', fontfamily='serif')
+    ax5.text(0, -0.2, 'Midwest', fontsize=15, fontweight='bold', fontfamily='serif')
 
-    #ax0.set_xticklabels(['Debt consolidation', 'redit card', 'home improvement','house', 'major purchase', 'small business','car','wedding','medical'])
-    #ax0.tick_params(bottom=True)
+    ax0.set_xticklabels("")
+    ax0.tick_params(bottom=False)
+
     ax1.set_xticklabels("")
     ax1.set_yticklabels("")
     ax1.tick_params(left=False)
-    # ax1.tick_params(bottom=False)
+    ax1.tick_params(bottom=False)
     # ax2.set_xticklabels("")
     # ax2.set_yticklabels("")
     # ax2.tick_params(left=False)
     # ax2.tick_params(bottom=False)
     # ax3.set_xticklabels(
-    #     ['Debt consolidation', 'redit card', 'home improvement','house', 'major purchase', 'small business','car','wedding','medical'],rotation=270)
-    # ax3.tick_params(left=False)
-    # ax4.set_xticklabels("")
-    # ax4.set_yticklabels("")
-    # ax4.tick_params(left=False)
-    # ax4.set_yticklabels("")
-    # ax4.tick_params(left=False)
+    # ['Debt consolidation', 'redit card', 'home improvement','house', 'major purchase', 'small business','car','wedding','medical']) #,rotation=270
+    ax3.tick_params(left=False)
+    ax4.set_xticklabels("")
+    ax4.set_yticklabels("")
+    ax4.tick_params(left=False)
+    ax4.set_yticklabels("")
+    ax4.tick_params(left=False)
     # ax4.set_xticklabels(
-    #     ['Debt consolidation', 'redit card', 'home improvement', 'house', 'major purchase', 'small business', 'car','wedding', 'medical'],rotation=270)
-    # ax5.set_yticklabels("")
-    # ax5.tick_params(left=False)
+    # ['Debt consolidation', 'redit card', 'home improvement', 'house', 'major purchase', 'small business', 'car','wedding', 'medical'])#,rotation=270)
+    ax5.set_yticklabels("")
+    ax5.tick_params(left=False)
     # ax5.set_xticklabels(
-    #     ['Debt consolidation', 'redit card', 'home improvement','house', 'major purchase', 'small business','car','wedding','medical'],rotation=270)
-    # ax0.set_xlabel("")
-    # ax1.set_xlabel("")
-    # ax1.set_ylabel("")
+    # ['Debt consolidation', 'redit card', 'home improvement','house', 'major purchase', 'small business','car','wedding','medical'])#,rotation=270)
+
+
+    ax0.set_xlabel("")
+    ax1.set_xlabel("")
+    ax1.set_ylabel("")
     # ax2.set_xlabel("")
     # ax2.set_ylabel("")
-    # ax4.set_ylabel("")
-    # ax5.set_ylabel("")
+    ax4.set_ylabel("")
+    ax5.set_ylabel("")
     plt.savefig("my_corrrelation.jpg", dpi=250, bbox_inches='tight')
     plt.show()
 
@@ -225,10 +321,10 @@ if __name__ == '__main__':
     #Specific_region =[]
 
 
-    for Region_selected in Regions:
-        Region_selected_df = df.loc[df['Region'] == Region_selected, :]
-        res = creating_the_dataframe_correlation_for_each_region(Region_selected_df,Region_selected)
-        creating_the_correlation_chart_for_each_region(res)
+    #for Region_selected in Regions:
+    #Region_selected_df = df.loc[df['Region'] == Region_selected, :]
+    res = creating_the_dataframe_correlation_for_each_region(df)
+    creating_the_correlation_chart_for_each_region(res)
 
 
 
