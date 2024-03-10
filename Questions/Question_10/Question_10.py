977e3bea7f6fdc4415b667b855b7b100b8a46aa5
AnalyticsForPleasure <shaycohen2003@gmail.com>
2024-03-10 17:00:20 +0200

    changing the path I wanted to go

--- Questions/Question_10/Question_10.py
+++ Questions/Question_10/Question_10.py
@@ -8,7 +8,18 @@ import squarify
 
 
 
-
+# **************************************************************************************************************
+# Function  name: get_later_date - Function to compare dates and return the later on
+# input:
+# return value:
+# ***************************************************************************************************************
+def get_later_date(row):
+    issue_date = pd.to_datetime(row['issue_date'], format='%d-%m-%Y')
+    last_credit_pull_date = pd.to_datetime(row['last_credit_pull_date'], format='%d-%m-%Y')
+    if issue_date > last_credit_pull_date:
+        return 'issue_date'
+    else:
+        return 'last_credit_pull_date'
 
 if __name__ == '__main__':
 
@@ -20,35 +31,45 @@ if __name__ == '__main__':
 
     # The last credit pull date is often close to the issue date
 
-    "issue date" and "last credit pull date"
-
     checking_connection = df.loc[:,["issue_date","last_credit_pull_date"]]
     print('*')
 
-    # Convert string columns to datetime objects
-    checking_connection['issue_date'] = pd.to_datetime(checking_connection['issue_date'], format='%d-%m-%Y')
-    checking_connection['last_credit_pull_date'] = pd.to_datetime(checking_connection['last_credit_pull_date'], format='%d-%m-%Y')
+    # Convert dates to string format "DD-MM-YYYY"
+    checking_connection['issue_date'] = checking_connection['issue_date'].dt.strftime('%d-%m-%Y')
+    checking_connection['last_credit_pull_date'] = checking_connection['last_credit_pull_date'].dt.strftime('%d-%m-%Y')
 
-    # Calculate the difference between the dates in days
-    checking_connection['days_difference'] = (checking_connection['last_credit_pull_date'] - checking_connection['issue_date']).dt.days
+    # Add a new column to the DataFrame with the later date
+    checking_connection['result_col'] = checking_connection.apply(get_later_date, axis=1)
 
-    # Define the bins
-    bins = [-300,-270, -240, -210, -180, -150, -120, -90, -60, -30, 0, 30, 60, 90, 120,150,180,210,240,270,300]
+    # Display first few rows of the DataFrame
+    print(df.head())
 
-    # Define labels for the bins
-    labels =['-300 to -270','-270 to -240','-240 to -210','-210 to -180' ,'-180 to -150','-150 to -120','-120 to -90', '-90 to -60', '-60 to -30', '-30 to 0', '0 to 30', '30 to 60', '60 to 90', '90 to 120','120 to 150','150 to 180','180 to 210','210 to 240','240 to 270','270 to 300']
 
-    # Create a new column with the bins
-    checking_connection['days_range'] = pd.cut(checking_connection['days_difference'], bins=bins, labels=labels, right=False)
-
-    # Display the DataFrame
-    print(checking_connection)
-
-    # Calculate the correlation between the two columns
-    correlation = checking_connection['issue_date'].corr(checking_connection['last_credit_pull_date'])
-
-    # Display the correlation coefficient
-    print("Correlation coefficient between issue_date and last_credit_pull_date:", correlation)
-    #days_difference_His = checking_connection['days_difference'].value_counts()
-    print('*')
+    # # Convert string columns to datetime objects
+    # checking_connection['issue_date'] = pd.to_datetime(checking_connection['issue_date'], format='%d-%m-%Y')
+    # checking_connection['last_credit_pull_date'] = pd.to_datetime(checking_connection['last_credit_pull_date'], format='%d-%m-%Y')
+    #
+    # # Calculate the difference between the dates in days
+    # checking_connection['days_difference'] = (checking_connection['last_credit_pull_date'] - checking_connection['issue_date']).dt.days
+    #
+    # # Define the bins
+    # bins = [-330,-300,-270, -240, -210, -180, -150, -120, -90, -60, -30, 0, 30, 60, 90, 120,150,180,210,240,270,300,330,360]
+    #
+    # # Define labels for the bins
+    # labels =['-330 to 300','-300 to -270','-270 to -240','-240 to -210','-210 to -180' ,'-180 to -150','-150 to -120','-120 to -90', '-90 to -60', '-60 to -30', '-30 to 0', '0 to 30', '30 to 60', '60 to 90', '90 to 120','120 to 150','150 to 180','180 to 210','210 to 240','240 to 270','270 to 300', '300 to 330','330 to 360']
+    #
+    # # Create a new column with the bins
+    # checking_connection['days_range'] = pd.cut(checking_connection['days_difference'], bins=bins, labels=labels, right=False)
+    # days_difference_His = checking_connection['days_range'].value_counts().reset_index()
+    # checking_connection.sort_values(by='days_range', inplace=True, ascending=False)
+    # # Display the DataFrame
+    # print(checking_connection)
+    #
+    # # Calculate the correlation between the two columns
+    # correlation = checking_connection['issue_date'].corr(checking_connection['last_credit_pull_date'])
+    #
+    # # Display the correlation coefficient
+    # print("Correlation coefficient between issue_date and last_credit_pull_date:", correlation)
+    # #
+    # print('*')
 
